43534aa0266cbd8576442ddd16dda68a6799d827
Menna Hassan <h.menna85@gmail.com>
2024-04-10 03:40:53 +0200

    updated

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-"""Instantiate the Babel object"""
+"""
+Basic Flask app with babel
+"""
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
 
 class Config:
-    """Config Babel"""
+    """AI is creating summary for Config class
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app)
-
+app.config.from_object(Config)
 
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
@@ -27,35 +29,45 @@ users = {
 
 @babel.localeselector
 def get_locale():
-    """get locale"""
-    local = request.args.get('locale')
-    if local and local in app.config['LANGUAGES']:
-        return local
+    """AI is creating summary for get_locale
+    """
+    if (request.args.get('locale')) and\
+       (request.args.get('locale') in app.config['LANGUAGES']):
+        return request.args.get('locale')
+
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 def get_user():
-    """get user"""
+    """AI is creating summary for get_user
+
+    Returns:
+        Union[Dict, None]: user dictionary if found otherwise None
+    """
     try:
-        return users[int(request.args.get('login_as'))]
+        return users.get(int(request.args.get('login_as')))
     except Exception:
         return None
 
 
 @app.before_request
 def before_request():
-    """before request"""
-    user = get_user()
-    if user:
-        g.user = user['name']
-    else:
-        g.user = None
+    """AI is creating summary for before_request
+    """
+    g.user = get_user()
 
 
 @app.route('/')
 def index():
-    """index page"""
-    username = g.user
+    """AI is creating summary for index
+
+    Returns:
+        str: html template
+    """
+    if g.user:
+        username = g.user['name']
+    else:
+        username = None
     return render_template('5-index.html', username=username)
 
 
