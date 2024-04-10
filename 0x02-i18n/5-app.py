d47d2159ea563a277d1ab7f389f518be3f5101e0
Menna Hassan <h.menna85@gmail.com>
2024-04-10 03:29:59 +0200

    updated

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -6,13 +6,6 @@ Basic Flask app with babel
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 
 class Config:
     """AI is creating summary for Config class
@@ -25,6 +18,13 @@ class Config:
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 @babel.localeselector
