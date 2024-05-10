b8e67901765246b41b5fdfb3d7e4fab8669b79d6
root <root@ubuntu>
2024-05-11 01:55:23 +0300

    task 0 and 1

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,22 @@
+import redis  from 'redis';
+
+const client = redis.createClient()
+  .on('connect', () => console.log('Redis client connected to the server'))
+  .on('error', err => console.log('Redis client not connected to the server: ', err));
+
+
+const setNewSchool = (schoolName, value) => {
+    client.set(schoolName, value, redis.print);
+}
+
+const displaySchoolValue = (schoolName) => {
+    client.get(schoolName, (err, reply) => {
+        console.log(reply);
+    });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+export default client;
